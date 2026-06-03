fd7453bc3d7aa6848474676873100c84a13ef261
kate931 <katya.vysochina2007@mail.ru>
2026-06-03 14:44:18 +0300

    2 задание

--- js/script.js
+++ js/script.js
@@ -17,4 +17,4 @@ document.addEventListener("DOMContentLoaded", function(){
     closeModalButton.addEventListener("click", function() {
         modalApplication.setAttribute("hidden", "true");
     });
-});
+}); 
